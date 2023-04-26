4362b2858a35bcf6e4bce3e49159914671bf3fb8
John Torres <jjtcsuf@gmail.com>
2023-04-25 18:51:32 -0700

    feat: set colorscheme to catppuccin-mocha

--- init.lua
+++ init.lua
@@ -112,11 +112,12 @@ require('lazy').setup({
     },
   },
 
-  { -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 
@@ -126,7 +127,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'catpuccin-mocha',
         component_separators = '|',
         section_separators = '',
       },
