2b1dd8a0489617a3569042f100d4ba5cff11cdb3
John Torres <jjtcsuf@gmail.com>
2023-04-26 08:20:57 -0700

    feat: use whichkey register for search

--- init.lua
+++ init.lua
@@ -288,11 +288,15 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+local wk = require('which-key')
+wk.register({
+  ['<leader>f'] = { name = '+find' },
+  ['<leader>ff'] = { require('telescope.builtin').find_files, 'Find Files' },
+  ['<leader>fh'] = { require('telescope.builtin').help_tags, 'Find Help' },
+  ['<leader>fw'] = { require('telescope.builtin').grep_string, 'Find Current Word' },
+  ['<leader>fg'] = { require('telescope.builtin').live_grep, 'Find by Grep' },
+  ['<leader>fd'] = { require('telescope.builtin').diagnostics, 'Find Diagnostics' },
+})
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
